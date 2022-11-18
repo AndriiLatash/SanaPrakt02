f947be1b04b78ece8cd08efff97d38731a4a0e2c
Andrii <latasandrej@gmail.com>
2022-11-19 00:15:24 +0200

    Cganged 4, 5 lines

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -2,6 +2,6 @@
 Console.WriteLine("Курси ");
 Console.WriteLine("Sana");
 Console.WriteLine("Commerce");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Software");
+Console.WriteLine("Development Bootcamp");
 
